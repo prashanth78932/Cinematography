c6eaf90cffc2100c34749841d3239068da2f9981
prashanth78932 <127941699+prashanth78932@users.noreply.github.com>
2023-06-10 18:51:12 +0530

    Add files via upload

---
+++ javascript/cinematography.js
@@ -0,0 +1,6 @@
+function header()
+{
+    document.getElementById("title").style.transform="translate(80vw,0vh)";
+    document.getElementById("title").style.transition="transform 0.5s"
+}
+var a=setTimeout(header,500)
